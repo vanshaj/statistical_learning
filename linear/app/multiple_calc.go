41f5cb87574ec5449b2b2e12ff8f76a6e0d17049
vanshaj <vanshajdhar@gmail.com>
2024-09-27 09:40:07 +0000

    Added correct code to predict coefficient for multiple regression

--- linear/app/multiple_calc.go
+++ linear/app/multiple_calc.go
@@ -6,28 +6,41 @@ import (
 	"github.com/vanshaj/statistical_learning/linear/internal"
 )
 
+/*
+the first column will be initiliazed with 1
+Then each column will be initiliazed with the column values of that variable
+*/
 func calculateMultipleRegressionCoefficient(x_query []string, y_query string) (*internal.MultipleCoeff, error) {
-	xData := make([]float64, 0)
+	yData, err := db.GetValues(y_query)
+	if err != nil {
+		return nil, err
+	}
+	// Number of rows will be the number of response values
+	xData := make([][]float64, len(yData))
 	for i, cur_query := range x_query {
 		value, err := db.GetValues(cur_query)
 		if err != nil {
 			return nil, err
 		}
-		if i == 0 {
-			one_slice := make([]float64, len(value))
-			for j := 0; j < len(value); j++ {
-				one_slice[j] = 1
+		for j := 0; j < len(value); j++ {
+			if i == 0 {
+				// Number of cols for each row will be number of predictors + 1 as first column will be initiliazed with 1
+				xData[j] = make([]float64, len(x_query)+1)
+				xData[j][0] = 1
 			}
-			xData = append(xData, one_slice...)
+			xData[j][i+1] = value[j]
 		}
-		xData = append(xData, value...)
-	}
-	yData, err := db.GetValues(y_query)
-	if err != nil {
-		return nil, err
 	}
 	m := internal.NewMultipleCoeff()
-	m.CalculateCoefficient(xData, yData, len(yData), len(x_query)+1)
+	xSingData := make([]float64, len(yData)*(len(x_query)+1))
+	var count int
+	for i := 0; i < len(yData); i++ {
+		for j := 0; j < len(x_query)+1; j++ {
+			xSingData[count] = xData[i][j]
+			count++
+		}
+	}
+	m.CalculateCoefficient(xSingData, yData, len(yData), len(x_query)+1)
 	for _, val := range m.Coefficients {
 		log.Debug().Msgf("Coefficients predicted are %f\n", val)
 	}
--- linear/internal/ml_ols.go
+++ linear/internal/ml_ols.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"github.com/rs/zerolog/log"
 	"gonum.org/v1/gonum/mat"
 )
 
@@ -22,17 +21,30 @@ func (m *MultipleCoeff) CalculateCoefficient(x, y []float64, n, p int) {
 	*/
 	Y_mat := mat.NewDense(n, 1, y)
 	X_mat := mat.NewDense(n, p, x)
+	// fa := mat.Formatted(X_mat, mat.Prefix("   "), mat.Squeeze())
+	// fmt.Printf("X matrix %v\n", fa)
+
+	/*
+	 X = [n*p] matrix
+	 X^T = [p*n] matrix
+	 X^T * X = [p*p] matrix
+	*/
 	X_out := mat.NewDense(p, p, make([]float64, p*p))
 	var X_t mat.Matrix = X_mat.T()
-	t_r, t_c := X_t.Dims()
-	log.Debug().Msgf("Dimensions of X Transpose : %d, %d", t_r, t_c)
-	log.Debug().Msgf("Dimensions of X: %d, %d", n, p)
-	t_r, t_c = X_out.Dims()
-	log.Debug().Msgf("Dimensions of X output: %d, %d", t_r, t_c)
 	X_out.Mul(X_t, X_mat)
+
+	// Inverse of (X^T*X)-1
 	X_out.Inverse(X_out)
+
+	// X^T * X = [p*p] matrix so inverse will also be p*p
+	// X^T = [p*n] matrix
+	// so multiplying both will result in [p*n] matrix
 	X_out_2 := mat.NewDense(p, n, make([]float64, p*n))
 	X_out_2.Mul(X_out, X_t)
+
+	// last result is [p*n] matrix
+	// Y is [n*1] matrix
+	// so multiplying both will result in p*1 matrix
 	X_out_3 := mat.NewDense(p, 1, make([]float64, p*1))
 	X_out_3.Mul(X_out_2, Y_mat)
 
