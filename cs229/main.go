f8be36e1449e76ed4fb9c26514a00bbda060f1e2
vanshaj <vanshaj.dhar@gmail.com>
2025-07-27 13:08:24 +0530

    Added code for logistic regression using Newton Method and Gradient Descent

--- cs229/main.go
+++ cs229/main.go
@@ -1,12 +1,51 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/vanshaj/cs229/ps1"
+	"gonum.org/v1/gonum/mat"
 )
+
+func readFile(filename string) (*mat.Dense, *mat.Dense) {
+	log.Printf("Reading file %s\n", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	csvReader := csv.NewReader(file)
+	xFeatures := make([]float64, 0, 2400)
+	yFeatures := make([]float64, 0, 800)
+	record, err := csvReader.ReadAll()
+	//log.Printf("Total number of examples are %d\n", len(record))
+	for i := range len(record) {
+		for j := range 3 { // as there are 3 columns
+			if i == 0 {
+				continue
+			}
+			val, err := strconv.ParseFloat(record[i][j], 64)
+			if err != nil {
+				//log.Fatalf("error parsing %s\n", err)
+			}
+			if j == 3-1 {
+				xFeatures = append(xFeatures, 1.0)
+				yFeatures = append(yFeatures, float64(val))
+			} else {
+				xFeatures = append(xFeatures, float64(val))
+			}
+		}
+	}
+	log.Printf("Converting file %s to matrix\n", filename)
+	log.Printf("Size of array is %d, and dimension of matrix is %d*%d\n", len(xFeatures), len(yFeatures), 3)
+	x := mat.NewDense(len(yFeatures), 3, xFeatures)
+	y := mat.NewDense(len(yFeatures), 1, yFeatures)
+	return x, y
+}
 
 func main() {
 	fmt.Println("Regression YaYYYYY")
@@ -16,5 +55,7 @@ func main() {
 		log.Fatalf("Unable to generate absolute file path for file %s due to  %s", err, filename)
 	}
 	l := ps1.LogisticRegression{}
-	l.Fit(false, absFilePath)
+	x, y := readFile(absFilePath)
+	l.Fit(true, x, y)
+	l.Fit(false, x, y)
 }
--- cs229/ps1/logistic_regression.go
+++ cs229/ps1/logistic_regression.go
@@ -1,11 +1,10 @@
 package ps1
 
 import (
-	"encoding/csv"
 	"log"
+
+	//"log"
 	"math"
-	"os"
-	"strconv"
 
 	"gonum.org/v1/gonum/mat"
 )
@@ -13,9 +12,9 @@ import (
 type LogisticRegression struct{}
 
 func (l *LogisticRegression) hypothesis(theta, x *mat.Dense) *mat.Dense {
-	log.Println("Calculating Hypothesis for LogisticRegression...")
-	numExamples, numFeatures := x.Dims()
-	log.Printf("Number of examples in the data set are %d, and number of features are %d\n", numExamples, numFeatures)
+	//log.Println("Calculating Hypothesis for LogisticRegression...")
+	numExamples, _ := x.Dims()
+	//log.Printf("Number of examples in the data set are %d, and number of features are %d\n", numExamples, numFeatures)
 	ePow := mat.NewDense(numExamples, 1, nil)
 	ePow.Mul(x, theta)
 	y := mat.NewDense(numExamples, 1, nil)
@@ -24,12 +23,12 @@ func (l *LogisticRegression) hypothesis(theta, x *mat.Dense) *mat.Dense {
 		value := 1 / denominator
 		y.Set(i, 0, value)
 	}
-	log.Printf("Calculated the Hypothesis values and dimensions are %d*1\n", numExamples)
+	//log.Printf("Calculated the Hypothesis values and dimensions are %d*1\n", numExamples)
 	return y
 }
 
 func (l *LogisticRegression) gradient(theta, x, y *mat.Dense) *mat.Dense {
-	log.Println("Calculating gradient for the LogisticRegression...")
+	//log.Println("Calculating gradient for the LogisticRegression...")
 	hypothesis := l.hypothesis(theta, x)
 	numExamples, numFeatures := x.Dims()
 	errorMLE := mat.NewDense(numExamples, 1, nil)
@@ -45,16 +44,50 @@ func (l *LogisticRegression) gradient(theta, x, y *mat.Dense) *mat.Dense {
 	return finalGradeint
 }
 
-func (l *LogisticRegression) Hessian() {}
+func (l *LogisticRegression) hessian(theta, x *mat.Dense) *mat.Dense {
+	numSamples, numFeatures := x.Dims()
+	hypoth := l.hypothesis(theta, x)
+	dMatrix := mat.NewDense(numSamples, numSamples, nil)
+	for i := range numSamples {
+		dMatrix.Set(i, i, hypoth.At(i, 0)*(1-hypoth.At(i, 0)))
+	}
+	transposeX := x.T()
+	firstPart := mat.NewDense(numFeatures, numSamples, nil)
+	firstPart.Mul(transposeX, dMatrix)
+	secondPart := mat.NewDense(numFeatures, numFeatures, nil)
+	secondPart.Mul(firstPart, x)
+	for i := range numFeatures {
+		for j := range numFeatures {
+			secondPart.Set(i, j, (1.0/float64(numSamples))*secondPart.At(i, j))
+		}
+	}
+	return secondPart
+}
 
-func (l *LogisticRegression) fitUsingNewton() {}
-
-func (l *LogisticRegression) fitGradient(x, y *mat.Dense) *mat.Dense {
-	learningRate := 0.0001
+func (l *LogisticRegression) fitUsingNewton(x, y *mat.Dense) *mat.Dense {
 	_, numFeatures := x.Dims()
 	theta := mat.NewDense(numFeatures, 1, nil)
-	gradi := l.gradient(theta, x, y)
-	for _ = range 8000 {
+	for _ = range 100 {
+		gradi := l.gradient(theta, x, y)
+		hess := l.hessian(theta, x)
+		hessInv := mat.NewDense(numFeatures, numFeatures, nil)
+		err := hessInv.Inverse(hess)
+		if err != nil {
+			panic(err)
+		}
+		gradiMul := mat.NewDense(numFeatures, 1, nil)
+		gradiMul.Mul(hessInv, gradi)
+		theta.Sub(theta, gradiMul)
+	}
+	return theta
+}
+
+func (l *LogisticRegression) fitGradient(x, y *mat.Dense) *mat.Dense {
+	learningRate := 0.001
+	_, numFeatures := x.Dims()
+	theta := mat.NewDense(numFeatures, 1, nil)
+	for _ = range 1000000 {
+		gradi := l.gradient(theta, x, y)
 		gradiMul := mat.NewDense(numFeatures, 1, nil)
 		for i := range numFeatures {
 			value := learningRate * gradi.At(i, 0)
@@ -65,46 +98,11 @@ func (l *LogisticRegression) fitGradient(x, y *mat.Dense) *mat.Dense {
 	return theta
 }
 
-func (l *LogisticRegression) Fit(isNewton bool, filename string) {
-	log.Printf("Reading file %s\n", filename)
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Printf("Unable to open file %s\n", filename)
-		return
-	}
-	csvReader := csv.NewReader(file)
-	xFeatures := make([]float64, 0, 100000)
-	yFeatures := make([]float64, 0, 1000)
-	record, err := csvReader.ReadAll()
-	log.Printf("Total number of examples are %d\n", len(record))
-	for j := range 3 { // as there are 3 columns
-		for i := range len(record) {
-			if i == 0 {
-				continue
-			}
-			val, err := strconv.ParseFloat(record[i][j], 64)
-			if err != nil {
-				log.Fatalf("error parsing %s\n", err)
-			}
-			if j == 3-1 {
-				xFeatures = append(xFeatures, 0.0)
-				yFeatures = append(yFeatures, float64(val))
-			} else {
-				xFeatures = append(xFeatures, float64(val))
-			}
-		}
-	}
-	log.Printf("Converting file %s to matrix\n", filename)
-	log.Printf("Size of array is %d, and dimension of matrix is %d*%d\n", len(xFeatures), len(yFeatures), 3)
-	x := mat.NewDense(len(yFeatures), 3, xFeatures)
-	y := mat.NewDense(len(yFeatures), 1, yFeatures)
-	/*
-		log.Printf("Feature vector = %v\n", mat.Formatted(x, mat.Prefix("    "), mat.Squeeze()))
-		log.Printf("Output vector = %v\n", mat.Formatted(y, mat.Prefix("    "), mat.Squeeze()))
-	*/
+func (l *LogisticRegression) Fit(isNewton bool, x, y *mat.Dense) {
 	if isNewton {
 		log.Println("Using newton method")
-		// l.fitUsingNewton()
+		theta := l.fitUsingNewton(x, y)
+		log.Printf("Output vector = %v\n", mat.Formatted(theta, mat.Prefix("    "), mat.Squeeze()))
 	} else {
 		log.Println("Using gradient descent")
 		theta := l.fitGradient(x, y)
