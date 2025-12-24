4e8e90415a304baab63bab85a2ff45b72cd3c8bf
vanshaj <vanshajdhar@gmail.com>
2025-12-24 09:56:29 +0530

    Added ReLU code

--- cs229/main.go
+++ cs229/main.go
@@ -9,7 +9,21 @@ import (
 
 func main() {
 	//prediction.PredictPoisson()
-	forwardPass()
+	// neuralnet.ForwardPassExample()
+	withDenseLayers()
+}
+
+func withDenseLayers() {
+	h1 := neuralnet.NewHidderLayer(3, 4)
+	inputF := []float64{
+		1, 2, 3, 2.5,
+		2, 5, -1, 2.0,
+		-1.5, 2.7, 3.3, -0.8,
+	}
+	outputL1 := h1.CalculateForwardPassOutput(inputF, 3)
+	h2 := neuralnet.NewHidderLayer(3, 3)
+	outputL2 := h2.CalculateForwardPassOutputMat(outputL1)
+	fmt.Printf("%v\n", mat.Formatted(outputL2, mat.Prefix("  ")))
 }
 
 func simpleNeuron() {
@@ -37,54 +51,5 @@ func simpleNeuron() {
 	for i := range len(outputs) {
 		fmt.Println(outputs[i])
 	}
-
-}
-
-func forwardPass() {
-	// We have 3 set of inputs and each input has 4 values
-	inputF := []float64{
-		1, 2, 3, 2.5,
-		2, 5, -1, 2.0,
-		-1.5, 2.7, 3.3, -0.8,
-	}
-	/* Assuming we have 3 neurons
-	Each has 4 weights for each value of 1 input set
-	*/
-	weightsF := []float64{
-		0.2, 0.8, -0.5, 1,
-		0.5, -0.91, 0.26, -0.5,
-		-0.26, -0.27, 0.17, 0.87,
-	}
-	/*
-		We have 3 neurons and each has one bias
-	*/
-	biasF := []float64{2, 3, 0.5}
-	hl := neuralnet.HiddenLayer{
-		Weights:          weightsF,
-		Bias:             biasF,
-		NeuronCount:      3, // Number of neurons in this layer
-		WeightInputCount: 4, // weight for neuron per input as we have 4 inputs in each set
-	}
-	outputMat := hl.CalculateForwardPassOutput(inputF, 3) // output will be 3 * 3 matrix as we have 3 neurons
-	// fmt.Printf("%v\n", mat.Formatted(outputMat, mat.Prefix("  ")))
-
-	/*
-		For new layers let's assume we have 3 more neurons
-		And has 3 weights for each value of 1 ouput
-	*/
-	weightsF2 := []float64{
-		0.1, -0.14, 0.5,
-		-0.5, 0.12, -0.33,
-		-0.44, 0.73, -0.13,
-	}
-	biasF2 := []float64{-1, 2, -0.5}
-	hl2 := neuralnet.HiddenLayer{
-		Weights:          weightsF2,
-		Bias:             biasF2,
-		NeuronCount:      3,
-		WeightInputCount: 3,
-	}
-	finalOutputMat := hl2.CalculateForwardPassOutputMat(outputMat)
-	fmt.Printf("%v\n", mat.Formatted(finalOutputMat, mat.Prefix("  ")))
 
 }
--- cs229/pkg/neuralnet/neuronMat.go
+++ cs229/pkg/neuralnet/neuronMat.go
@@ -1,6 +1,7 @@
 package neuralnet
 
 import (
+	"fmt"
 	"math/rand"
 
 	"gonum.org/v1/gonum/mat"
@@ -13,9 +14,59 @@ type HiddenLayer struct {
 	WeightInputCount int // number of weights for neuron per input as we have
 }
 
+func ForwardPassExample() {
+	// We have 3 set of inputs and each input has 4 values
+	inputF := []float64{
+		1, 2, 3, 2.5,
+		2, 5, -1, 2.0,
+		-1.5, 2.7, 3.3, -0.8,
+	}
+	/* Assuming we have 3 neurons
+	Each has 4 weights for each value of 1 input set
+	*/
+	weightsF := []float64{
+		0.2, 0.8, -0.5, 1,
+		0.5, -0.91, 0.26, -0.5,
+		-0.26, -0.27, 0.17, 0.87,
+	}
+	/*
+		We have 3 neurons and each has one bias
+	*/
+	biasF := []float64{2, 3, 0.5}
+	hl := HiddenLayer{
+		Weights:          weightsF,
+		Bias:             biasF,
+		NeuronCount:      3, // Number of neurons in this layer
+		WeightInputCount: 4, // weight for neuron per input as we have 4 inputs in each set
+	}
+	outputMat := hl.CalculateForwardPassOutput(inputF, 3) // output will be 3 * 3 matrix as we have 3 neurons
+	// fmt.Printf("%v\n", mat.Formatted(outputMat, mat.Prefix("  ")))
+
+	/*
+		For new layers let's assume we have 3 more neurons
+		And has 3 weights for each value of 1 ouput
+	*/
+	weightsF2 := []float64{
+		0.1, -0.14, 0.5,
+		-0.5, 0.12, -0.33,
+		-0.44, 0.73, -0.13,
+	}
+	biasF2 := []float64{-1, 2, -0.5}
+	hl2 := HiddenLayer{
+		Weights:          weightsF2,
+		Bias:             biasF2,
+		NeuronCount:      3,
+		WeightInputCount: 3,
+	}
+	finalOutputMat := hl2.CalculateForwardPassOutputMat(outputMat)
+	fmt.Printf("%v\n", mat.Formatted(finalOutputMat, mat.Prefix("  ")))
+
+}
+
 func NewHidderLayer(n_Neurons, n_Input int) *HiddenLayer {
 	weights := make([]float64, n_Input*n_Neurons)
 	bias := make([]float64, n_Neurons)
+	// Will create weights and bias automatically
 	for i := range n_Input * n_Neurons {
 		weights[i] = 0.01 * rand.Float64()
 	}
@@ -30,9 +81,31 @@ func NewHidderLayer(n_Neurons, n_Input int) *HiddenLayer {
 	}
 }
 
+func (hl *HiddenLayer) ActivationSoftmax(inputMat *mat.Dense) {
+	r, c := inputMat.Dims()
+	// First we will get max from each row
+	maxMat := mat.NewDense(r, c, nil)
+}
+
+// First we will apply ReLU and then we will go to forward Pass
+func (hl *HiddenLayer) ActivationReLU(inputMat *mat.Dense) {
+	// Apply will apply your func on each input of the Dense (matrix)
+	inputMat.Apply(func(r, c int, v float64) float64 {
+		if v > 0 {
+			return v
+		} else {
+			return 0
+		}
+	}, inputMat)
+}
+
 func (hl *HiddenLayer) CalculateForwardPassOutputMat(inputMat *mat.Dense) *mat.Dense {
 	inputRowCount, _ := inputMat.Dims()
 	weightsMat := mat.NewDense(hl.NeuronCount, hl.WeightInputCount, hl.Weights) // Weight matrix of x * n size
+
+	// Apply ReluActivation
+	hl.ActivationReLU(inputMat)
+
 	biasExpand := make([]float64, hl.NeuronCount*len(hl.Bias))
 	for i := range biasExpand {
 		index := i % hl.NeuronCount
@@ -51,24 +124,6 @@ func (hl *HiddenLayer) CalculateForwardPassOutputMat(inputMat *mat.Dense) *mat.D
 }
 
 func (hl *HiddenLayer) CalculateForwardPassOutput(input []float64, inputCount int) *mat.Dense {
-	inputMat := mat.NewDense(inputCount, hl.WeightInputCount, input)            // Input matrix of m * n size
-	weightsMat := mat.NewDense(hl.NeuronCount, hl.WeightInputCount, hl.Weights) // Weight matrix of x * n size
-
-	// Bias is of 1 * x but we want to later add it so we make it n*x
-	// We duplicated elements of first row in second and then next till n
-	biasExpand := make([]float64, hl.NeuronCount*len(hl.Bias))
-	for i := range biasExpand {
-		index := i % hl.NeuronCount
-		biasExpand[i] = hl.Bias[index]
-	}
-	biasExpandMat := mat.NewDense(inputCount, hl.NeuronCount, biasExpand)
-
-	// Transpose weight to make it n * x size
-	weightsMatT := weightsMat.T()
-
-	// Calculate output i.e m * x size
-	outputMat := mat.NewDense(inputCount, hl.NeuronCount, nil)
-	outputMat.Mul(inputMat, weightsMatT)
-	outputMat.Add(outputMat, biasExpandMat)
-	return outputMat
+	inputMat := mat.NewDense(inputCount, hl.WeightInputCount, input) // Input matrix of m * n size
+	return hl.CalculateForwardPassOutputMat(inputMat)
 }
